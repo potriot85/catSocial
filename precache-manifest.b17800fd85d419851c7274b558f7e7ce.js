4ad80c621d4559f923303a3bb6b0ea90dfdb83d7
potriot85 <potriot85@gmail.com>
2022-05-18 19:53:30 +0300

    Updates

---
+++ precache-manifest.b17800fd85d419851c7274b558f7e7ce.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b738d68337ffbea2c60",
+    "url": "/catSocial/static/css/main.3e1f2e5b.chunk.css"
+  },
+  {
+    "revision": "2b738d68337ffbea2c60",
+    "url": "/catSocial/static/js/main.2b738d68.chunk.js"
+  },
+  {
+    "revision": "a073953dc6003e92079e",
+    "url": "/catSocial/static/js/1.a073953d.chunk.js"
+  },
+  {
+    "revision": "47419fac412021b75b26",
+    "url": "/catSocial/static/js/runtime~main.47419fac.js"
+  },
+  {
+    "revision": "8ad070771555cf307ddd23338fec6c4c",
+    "url": "/catSocial/static/media/header.8ad07077.jpg"
+  },
+  {
+    "revision": "2f626a4eacd3b1741feabee111896f2c",
+    "url": "/catSocial/static/media/preloader.2f626a4e.gif"
+  },
+  {
+    "revision": "7b9ffdcff8786b25729bfbc3869888b7",
+    "url": "/catSocial/static/media/logo.7b9ffdcf.jpg"
+  },
+  {
+    "revision": "902fd1fdd7fff5a8cbc5fe559607506b",
+    "url": "/catSocial/static/media/nologo.902fd1fd.jpg"
+  },
+  {
+    "revision": "48d64ff78aa99e89ec1c0318cb2c9344",
+    "url": "/catSocial/static/media/bg.48d64ff7.jpg"
+  },
+  {
+    "revision": "1a03792113bedc498c05d6f0324e66e4",
+    "url": "/catSocial/index.html"
+  }
+];
